7cdf193b192a36aab7ad6015482352a56d9b09fc
craig91 <craigdejean1@gmail.com>
2022-12-27 19:38:01 -0500

    added qt project folder

---
+++ qt-projects/build-untitled-Desktop-Debug/CMakeFiles/untitled.dir/untitled_autogen/mocs_compilation.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/untitled.dir/untitled_autogen/mocs_compilation.cpp.o: \
+ /home/cdejean/Code/Cplus-stuff/qt-projects/build-untitled-Desktop-Debug/untitled_autogen/mocs_compilation.cpp \
+ /usr/include/stdc-predef.h
