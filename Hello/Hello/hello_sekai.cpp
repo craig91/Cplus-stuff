db8d01ffa2f90da21409bf619da37d315331e26e
craig91 <craigdejean1@gmail.com>
2023-01-10 21:03:36 -0500

    Added new project

---
+++ Hello/Hello/hello_sekai.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// #include <string>
+// #include <vector>
+// #include <algorithm>
+// #include <cmath>
+
+using namespace std;
+
+
+int main() {
+	cout << "Hello Craig!\n";
+	// keep_window_open();
+	return 0;
+
+}
