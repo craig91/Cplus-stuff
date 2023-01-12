c48520da866eea5b88bd8fe74b624275205c5d65
craig91 <craigdejean1@gmail.com>
2023-01-11 20:43:36 -0500

    added primitive type sizes

--- Hello_Sekai/Hello_Sekai/hello_world.cpp
+++ Hello_Sekai/Hello_Sekai/hello_world.cpp
@@ -6,14 +6,38 @@
 
 using namespace std;
 
-inline void keep_window_open() {
-	char ch;
-	cin >> ch;
-}
-
 int main() {
-	cout << "Hello World!\n";
-	cout << "Here we go!";
-	keep_window_open();
-	return 0;
+	// Assign multiple variables to one line
+	//int x = 5, y = 6, z = 50;
+	//cout << x + y + z;
+
+	// int a, b, c;
+	// a = b = c = 50;
+	// cout << a + b + c;
+
+	// Constants
+	const int myNum = 15;
+	// myNum = 10; --> Will result in an error, a constant (const) is immutable, it cannot be changed.
+	// you should always declare the variable as a constant when you have values that are unlikely to change.
+
+	const int minutesPerHour = 60;
+	const float PI = 3.14;
+
+	int x, y;
+	int sum;
+	cout << "Type a number: ";
+	cin >> x;
+	cout << "Type another number: ";
+	cin >> y;
+	sum = x + y;
+	cout << "Sum is: " << sum;
+
+	cout << "size of char: " << sizeof(char) << endl;
+	cout << "size of int: " << sizeof(int) << endl;
+	cout << "size of long: " << sizeof(long) << endl;
+	cout << "size of float: " << sizeof(float) << endl;
+	cout << "size of double: " << sizeof(double) << endl;
+
+
+
 }
