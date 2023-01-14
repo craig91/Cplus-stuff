9430302c013d4024d5b6b9e8d8509d03482d95e9
craig91 <craigdejean1@gmail.com>
2023-01-14 15:46:03 -0500

    added example function for clarity

--- functions/functions/main.cpp
+++ functions/functions/main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 void myFunction() {
 	cout << "I just got executed!" << endl;
 }
+
+void exFunction();
+
 int main() {
 
 	// Functions
@@ -16,9 +19,16 @@ int main() {
 
 
 	myFunction();
-	myFunction();
-	myFunction();
+	exFunction();
 
 	// The function can be declared as many times as you want.
 
+	// If a user defined function is declared after the main() function , an error will occur, what you can do is make the declartion above the main() function and define the function under it for code clarity as demonstrated with my exFunction().
+
+}
+
+void exFunction() {
+	int a = 5;
+	int b = 6;
+	cout << a + b << endl;
 }
