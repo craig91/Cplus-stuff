7ca24ac7a7e10a12295bb53d207445cb6a6412fd
craig91 <craigdejean1@gmail.com>
2023-01-15 10:36:00 -0500

    added function with return value and additional functions

--- functions/functions/main.cpp
+++ functions/functions/main.cpp
@@ -12,6 +12,19 @@ void nameFunction(string fname) {
 	cout << fname << " Dejean" << endl;
 };
 
+void countryFunction(string country = "Norway") {
+	cout << country << endl;
+}
+
+void multiParam(string name, int age) {
+	cout << name << " Dejean " << age << " Years old." << endl;
+}
+
+int returnValue(int x) {
+	return 5 + x;
+}
+
+
 int main() {
 
 	// Functions
@@ -29,8 +42,22 @@ int main() {
 
 	// If a user defined function is declared after the main() function , an error will occur, what you can do is make the declartion above the main() function and define the function under it for code clarity as demonstrated with my exFunction().
 
+	// One parameter
 	nameFunction("Craig");
 	nameFunction("Stephane");
+
+	// Default Parameters
+	countryFunction();
+	countryFunction("USA");
+	countryFunction("Zimbabwe");
+
+	// Multiple Parameters
+	multiParam("Craig", 31);
+	multiParam("Minami", 35);
+
+	// Functions with return values.
+	cout << returnValue(10);
+	cout << returnValue(1234);
 
 }
 
