1d3657780f95fbf850c6906ebe26be176324214e
craig91 <craigdejean1@gmail.com>
2023-01-24 20:26:15 -0500

    Added comment explaining #pragma once

--- header-practice/header-practice/Log.h
+++ header-practice/header-practice/Log.h
@@ -1,4 +1,4 @@
-#pragma once
+#pragma once  // This says to include the header file once. This functions as a header guide. It prevents including a single header file multiple times into a single translation unit. To emphasize, it DOES NOT prevent me from including the header file to different parts of my program, it only prevents including the file multiple times into a single translation unit.
 
 void Log(const char* message);
 void InitLog();
